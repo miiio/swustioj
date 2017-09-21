c7826cdec987863619a1d64ab98d948e0cc8e5d8
志平 朱 <18113928120@189.cn>
2017-09-21 18:34:11 +0800

    添加Butterknifte

--- app/src/main/java/cn/example/ioj/view/activity/impl/LoginActivity.java
+++ app/src/main/java/cn/example/ioj/view/activity/impl/LoginActivity.java
@@ -2,8 +2,12 @@ package cn.example.ioj.view.activity.impl;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.widget.ImageView;
 
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.example.ioj.R;
 import cn.example.ioj.presenter.impl.LoginPresenter;
 import cn.example.ioj.view.activity.i.ILoginActivity;
@@ -12,7 +16,20 @@ import cn.example.ioj.view.activity.i.ILoginActivity;
  * Created by Tolean on 2017/9/21.
  */
 
-public class LoginActivity extends BaseActivity<LoginPresenter>implements ILoginActivity {
+public class LoginActivity extends BaseActivity<LoginPresenter> implements ILoginActivity {
+    @BindView(R.id.et_login_username)
+    TextInputEditText mEtLoginUsername;
+    @BindView(R.id.til_login_username)
+    TextInputLayout mTilLoginUsername;
+    @BindView(R.id.im_login_clear_um)
+    ImageView mImLoginClearUm;
+    @BindView(R.id.et_login_pw)
+    TextInputEditText mEtLoginPw;
+    @BindView(R.id.im_login_clear_pw)
+    ImageView mImLoginClearPw;
+    @BindView(R.id.im_login_eyeable_pw)
+    ImageView mImLoginEyeablePw;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +44,9 @@ public class LoginActivity extends BaseActivity<LoginPresenter>implements ILogin
     /**
      * 初始化View
      */
-    private void initView(){
+    private void initView() {
         setContentView(R.layout.activity_login);
-
+        ButterKnife.bind(this);
     }
 
     @Override
