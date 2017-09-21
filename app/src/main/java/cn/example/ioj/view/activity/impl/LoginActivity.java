10cd8161db9b6434ab254e8b7dca9397b6883e8d
志平 朱 <18113928120@189.cn>
2017-09-21 17:08:48 +0800

    Merge branch 'master' of https://github.com/miiio/swustioj
    # Conflicts:
    #	app/src/main/java/cn/example/ioj/view/activity/impl/MainActivity.java

--- app/src/main/java/cn/example/ioj/view/activity/impl/LoginActivity.java
+++ app/src/main/java/cn/example/ioj/view/activity/impl/LoginActivity.java
@@ -1,8 +1,33 @@
 package cn.example.ioj.view.activity.impl;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+
+import cn.example.ioj.R;
+import cn.example.ioj.presenter.impl.LoginPresenter;
+import cn.example.ioj.view.activity.i.ILoginActivity;
+
 /**
  * Created by Tolean on 2017/9/21.
  */
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity<LoginPresenter>implements ILoginActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    @Override
+    protected LoginPresenter getPresenter() {
+        return new LoginPresenter(this);
+    }
+
+    /**
+     * 初始化View
+     */
+    private void initView(){
+        setContentView(R.layout.activity_login);
+    }
 }
